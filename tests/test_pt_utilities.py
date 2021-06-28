9884798a6565cb68f583f6e3740f8f7d7ad58d6f
Konrad Jałowiecki <dexter2206@gmail.com>
2021-06-28 06:54:25 +0200

    Move BQM related-utilities into separate module, add tests

--- tests/test_pt_utilities.py
+++ tests/test_pt_utilities.py
@@ -2,7 +2,7 @@
 import dimod
 import numpy as np
 
-from omnisolver.pt.sampler import vectorize_bqm
+from omnisolver.pt.bqm_tools import adjacency_list_from_couplings, vectorize_bqm
 
 
 class TestBQMVectorization:
@@ -39,3 +39,44 @@ class TestBQMVectorization:
 
         np.testing.assert_equal(h_vec, expected_h_vec)
         np.testing.assert_equal(j_mat, expected_j_mat)
+
+
+class TestAdjacencyListConstruction:
+    def test_neighbours_of_all_spins_in_dense_graph_are_correctly_identified(self):
+        j_mat = np.array(
+            [
+                [0.0, 3.0, -1.0, 2.0],
+                [3.0, 0.0, -0.5, -1.2],
+                [-1.0, -0.5, 0.0, -0.7],
+                [2.0, -1.2, -0.7, 0],
+            ]
+        )
+
+        expected_adjacency_list = np.array([[1, 2, 3], [0, 2, 3], [0, 1, 3], [0, 1, 2]])
+
+        expected_neighbours_count = 3 * np.ones(4)
+
+        adjacency_list, neighbours_count = adjacency_list_from_couplings(j_mat)
+
+        np.testing.assert_equal(neighbours_count, expected_neighbours_count)
+        np.testing.assert_equal(adjacency_list, expected_adjacency_list)
+
+    def test_neighbours_of_all_spins_in_sparse_graph_are_correctly_identified(self):
+        j_mat = np.array(
+            [
+                [0.0, 0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, -2.0, 0.0, 0.0],
+                [1.0, -2.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 0.5],
+                [0.0, 0.0, 0.0, 0.5, 0.0],
+            ]
+        )
+
+        expected_neighbours_count = np.array([1, 1, 2, 1, 1])
+
+        expected_adjacency_list = np.array([[2, 0], [2, 0], [0, 1], [4, 0], [3, 0]])
+
+        adjacency_list, neighbours_count = adjacency_list_from_couplings(j_mat)
+
+        np.testing.assert_equal(neighbours_count, expected_neighbours_count)
+        np.testing.assert_equal(adjacency_list, expected_adjacency_list)
