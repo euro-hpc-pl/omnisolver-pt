c13aa1400a422bc57533f0e01f6bb6fb211511fc
Konrad Jałowiecki <dexter2206@gmail.com>
2021-08-12 03:25:42 +0200

    Implement replica exchange

--- omnisolver/pt/algorithm.py
+++ omnisolver/pt/algorithm.py
@@ -1,6 +1,7 @@
 from typing import Sequence
 
 import numba
+import numpy as np
 
 from omnisolver.pt.replica import Replica
 
@@ -11,3 +12,11 @@ def perform_monte_carlo_sweeps(replicas: Sequence[Replica], num_sweeps):
         replica = replicas[i]
         for _ in range(num_sweeps):
             replica.perform_mc_sweep()
+
+
+@numba.njit
+def should_exchange_states(replica_1: Replica, replica_2: Replica):
+    exponent = (replica_1.current_energy - replica_2.current_energy) * (
+        replica_1.beta - replica_2.beta
+    )
+    return exponent > 0 or np.random.rand() < np.exp(exponent)
--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -1,8 +1,19 @@
+import numba
 import numpy as np
 
-from omnisolver.pt.algorithm import perform_monte_carlo_sweeps
+from omnisolver.pt.algorithm import perform_monte_carlo_sweeps, should_exchange_states
 from omnisolver.pt.model import ising_model
 from omnisolver.pt.replica import initialize_replica
+
+
+@numba.njit
+def numba_seed(seed):
+    np.random.seed(seed)
+
+
+@numba.njit
+def numba_rand():
+    return np.random.rand()
 
 
 class TestPerformingMonteCarloSweeps:
@@ -22,3 +33,55 @@ class TestPerformingMonteCarloSweeps:
         assert all(
             replica.current_energy < model.energy(initial_state) for replica in replicas
         )
+
+
+class TestReplicaExchangeCriterion:
+    def test_exchange_shifting_better_solution_to_colder_replica_is_always_accepted(
+        self,
+    ):
+        numba_seed(42)
+        model = ising_model(np.ones(3), np.zeros((3, 3)))
+        replica_1 = initialize_replica(model, np.ones(3, dtype=np.int8), beta=0.1)
+        replica_2 = initialize_replica(
+            model, np.array([-1, 1, -1], dtype=np.int8), beta=0.01
+        )
+
+        assert should_exchange_states(replica_1, replica_2)
+        assert should_exchange_states(replica_2, replica_1)
+
+    def test_moving_better_solution_to_hotter_replica_is_accepted_with_correct_probability(
+        self,
+    ):
+        # Idea of this test is similar to ones in test_replica.py
+        numba_seed(42)
+        model = ising_model(np.ones(3), np.zeros((3, 3)))
+        initial_state_1 = np.array([-1, -1, -1], dtype=np.int8)
+        initial_state_2 = np.array([1, 1, 1], dtype=np.int8)
+        energy_diff = model.energy(initial_state_1) - model.energy(initial_state_2)
+        threshold_beta_difference = np.log(numba_rand()) / energy_diff
+        beta_1 = 1.0
+        beta_2 = beta_1 - (threshold_beta_difference - 0.1)
+        numba_seed(42)
+
+        replica_1 = initialize_replica(model, initial_state_1, beta_1)
+        replica_2 = initialize_replica(model, initial_state_2, beta_2)
+
+        assert should_exchange_states(replica_1, replica_2)
+
+    def test_moving_better_solution_to_hotter_replica_is_rejected_with_correct_probability(
+        self,
+    ):
+        numba_seed(42)
+        model = ising_model(np.ones(3), np.zeros((3, 3)))
+        initial_state_1 = np.array([-1, -1, -1], dtype=np.int8)
+        initial_state_2 = np.array([1, -1, -1], dtype=np.int8)
+        energy_diff = model.energy(initial_state_1) - model.energy(initial_state_2)
+        threshold_beta_difference = np.log(numba_rand()) / energy_diff
+        beta_1 = 1.0
+        beta_2 = beta_1 - (threshold_beta_difference + 0.1)
+        numba_seed(42)
+
+        replica_1 = initialize_replica(model, initial_state_1, beta_1)
+        replica_2 = initialize_replica(model, initial_state_2, beta_2)
+
+        assert not should_exchange_states(replica_1, replica_2)
