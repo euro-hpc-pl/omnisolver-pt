82f32d0c77c4bcab5c4ac21d56e2cf11f63ae54c
Konrad Jałowiecki <dexter2206@gmail.com>
2021-08-12 02:49:23 +0200

    Implement performing parallel monte carlo sweeps for multiple replicas

--- omnisolver/pt/algorithm.py
+++ omnisolver/pt/algorithm.py
@@ -1,85 +1,13 @@
-"""Implementation of Parallel Tempering algorithm.
+from typing import Sequence
 
-The following choice of data structures has been made to make the implementation
-compatible with numba JIT (N denotes number of spins, M denotes number of replicas):
-
-- The Ising Model being solved is represented as bias vector h_vec and
-  couplings matrix j_mat.
-- Connectivity information is stored as adjacency list. Adjacency list consists of:
-  - a numpy array `neighbours_count` of shape (N,) declaring number of neighbours
-    of each spin
-  - a numpy array `adjacency_list` of shape (N, max(neighbours_count) such that
-    adjacency_list[i, j] is index of j-th neighbour of i-th spin.
-    If j >= neighbours_count[i], the adjacency_list[i, j] is undefined.
-- State of replicas is stored in (M, N) shaped array.
-- Energies of replicas are stored in (M,) shaped array.
-
-The choice of storage format for replicas allows for an efficient parallel access
-and makes it suitable to be used with numba.prange.
-"""
 import numba
-import numpy as np
+
+from omnisolver.pt.replica import Replica
 
 
-@numba.njit(fastmath=True)
-def energy_diff(
-    h_vec: np.ndarray,
-    j_mat: np.ndarray,
-    state: np.ndarray,
-    position: int,
-    neighbours_count: np.ndarray,
-    adjacency_list: np.ndarray,
-):
-    """Compute energy difference between two configurations differing by a spin flip.
-
-    Let H(s_1, ..., s_N) be Ising hamiltonian. Suppose we are given a configuration
-    (s_1, s_2,..., s_pos,...,s_N). This function computes the difference:
-
-    delta = H(s_1, s_2,...,s_pos,...,s_N) - H(s_1, s_2,...,-s_pos,...,s_N)
-
-    :param h_vec: vector of biases in Ising hamiltonian.
-    :param j_mat: a symmetric matrix of couplings in Ising hamiltonian.
-    :param state: a configuration of the system.
-    :param position: position of the spin to be flipped.
-    :param neighbours_count: array with number of neighbours of each spin. Refer to
-     this module's docstring for exact explanation of this argument.
-    :param adjacency_list: adjacency list for the graph the model is defined on.
-     Refer to this module's docstring for exact explanation of this argument.
-    :return: an energy change obtained from current configuration by flipping a
-     spin at `position`.
-    """
-    total = h_vec[position] * state[position]
-    for i in range(neighbours_count[position]):
-        j = adjacency_list[position, i]
-        total += state[position] * j_mat[position, j] * state[j]
-    return 2 * total
-
-
-@numba.njit(fastmath=True)
-def accept_solution(energy_diff, beta):
-    """Decide if solution with known energy difference should be accepted in MC move.
-
-    Solutions with better energy (energy_diff > 0) are always accepted.
-    Solutions with worse energy are accepted with probability exp(-beta * energy_diff)
-
-
-    .. note::
-        Since numba does not support numpy random number generator objects,
-        this function uses top-level numpy.random.random() function.
-        Should you require seeding of generator, use numpy.random.seed() to do so.
-
-    :param energy_diff: difference between old and proposed energy.
-    :param beta: inverse temperature.
-    :return: boolean indicating whether solution should be accepted.
-    """
-    return energy_diff > 0 or np.random.rand() < np.exp(beta * energy_diff)
-
-
-@numba.njit(fastmath=True)
-def energy(state, h_vec, j_mat):
-    total = 0.0
-    for i in range(j_mat.shape[0]):
-        total += state[i] * h_vec[i]
-        for j in range(i + 1, j_mat.shape[1]):
-            total += state[i] * state[j] * j_mat[i, j]
-    return total
+@numba.njit(parallel=True)
+def perform_monte_carlo_sweeps(replicas: Sequence[Replica], num_sweeps):
+    for i in numba.prange(len(replicas)):
+        replica = replicas[i]
+        for _ in range(num_sweeps):
+            replica.perform_mc_sweep()
--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -1,110 +1,24 @@
-import itertools
-
-import numba
 import numpy as np
-import pytest
 
-from omnisolver.pt.algorithm import accept_solution, energy, energy_diff
-from omnisolver.pt.bqm_tools import adjacency_list_from_couplings
-
-
-@numba.njit
-def _numba_seed(seed):
-    np.random.seed(seed)
+from omnisolver.pt.algorithm import perform_monte_carlo_sweeps
+from omnisolver.pt.model import ising_model
+from omnisolver.pt.replica import initialize_replica
 
 
-@numba.njit
-def _numba_random():
-    return np.random.rand()
+class TestPerformingMonteCarloSweeps:
+    def test_updates_all_replicas(self):
+        model = ising_model(np.array([1.0, 2.0, 3.0]), np.zeros((3, 3)))
+        # We choose the initial state with the worst energy. Hence, every sweep is guaranteed to
+        # improve the solution.
+        initial_state = np.ones(3, dtype=np.int8)
 
+        replicas = [
+            initialize_replica(model, initial_state, beta)
+            for beta in np.linspace(0.1, 1.0, 10)
+        ]
 
-def _energy(h_vec, j_mat, state):
-    return (h_vec * state).sum() + 0.5 * state @ j_mat @ state
+        perform_monte_carlo_sweeps(replicas, 1)
 
-
-def _random_ising(rng, size):
-    h_vec = rng.uniform(-2, 2, size)
-    j_mat = rng.uniform(-1, 1, (size, size))
-    j_mat += j_mat.T
-    j_mat[np.diag_indices(size)] = 0
-    return h_vec, j_mat
-
-
-def test_energy_is_computed_correctly_for_dense_instance():
-    h_vec, j_mat = _random_ising(np.random.default_rng(123), 10)
-
-    for configuration in itertools.product((-1, 1), repeat=10):
-        state = np.array(configuration)
-        assert energy(state, h_vec, j_mat) == pytest.approx(
-            _energy(h_vec, j_mat, state)
+        assert all(
+            replica.current_energy < model.energy(initial_state) for replica in replicas
         )
-
-
-class TestComputingEnergyDifference:
-    def test_difference_is_computed_correctly_for_position_of_isolated_spin(self):
-        # In the test case below spin 1 is isolated, and therefore the energy difference
-        # is equal, up to sign, to 2 * h_vec[1]
-        h_vec = np.array([1, 2, 3])
-        j_mat = np.array([[0.0, 0.0, -2.0], [0.0, 0.0, 0.0], [-2.0, 0.0, 0.0]])
-        neighbours_count = np.array([1, 0, 1])
-        adjacency_list = np.array([[2], [0], [0]])
-        state = np.array([1, -1, 1])
-
-        assert (
-            energy_diff(h_vec, j_mat, state, 1, neighbours_count, adjacency_list)
-            == -2 * h_vec[1]
-        )
-
-    @pytest.mark.parametrize("position", list(range(20)))
-    def test_difference_is_computed_correctly_for_position_of_non_isolated_spin(
-        self, position
-    ):
-        rng = np.random.default_rng(42)
-        h_vec, j_mat = _random_ising(rng, 20)
-        adjacency_list, neighbours_count = adjacency_list_from_couplings(j_mat)
-
-        state = 2 * (np.arange(20) % 2) - 1
-
-        flipped_state = state.copy()
-        flipped_state[position] = -flipped_state[position]
-
-        initial_energy = _energy(h_vec, j_mat, state)
-        flipped_energy = _energy(h_vec, j_mat, flipped_state)
-
-        assert energy_diff(
-            h_vec, j_mat, state, position, neighbours_count, adjacency_list
-        ) == pytest.approx(initial_energy - flipped_energy)
-
-        assert energy_diff(
-            h_vec, j_mat, flipped_state, position, neighbours_count, adjacency_list
-        ) == pytest.approx(flipped_energy - initial_energy)
-
-
-class TestMonteCarloMove:
-    def test_solution_with_lower_energy_is_always_accepted(self):
-        _numba_seed(42)
-        # Try several times, we should accept each time (since we shouldn't
-        # really use random numbers to decide it.
-        assert all(accept_solution(4.0, 10.0) for _ in range(100))
-
-    def test_solution_with_worse_energy_is_randomly_accepted_with_correct_probability(
-        self,
-    ):
-        _numba_seed(42)
-        # This is tricky, because we cannot inject random generator (see notes
-        # in docstring). The idea is, that instead we first generate random sample,
-        # and then construct negative and positive case for it.
-        beta = 0.1
-        u = _numba_random()
-
-        # Negative case
-        diff_negative = np.log(u) / beta - 1.0
-
-        # Positive case
-        diff_positive = np.log(u) / beta + 1.0
-
-        _numba_seed(42)
-        assert not accept_solution(diff_negative, beta)
-
-        _numba_seed(42)
-        assert accept_solution(diff_positive, beta)
